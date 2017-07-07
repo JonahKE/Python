558be371612798862af47fb153a86bf7ced5fa43
Isaac <isuntag@gmail.com>
2017-07-07 11:17:17 -0400

    Sorted into python_basics and flask projects

---
+++ python_basics/hospital.py
@@ -0,0 +1,6 @@
+class Patient(object):
+    def __init__(self, id, name, allergies, bed_num):
+        self.id = id
+        self.name = name
+        self.allergies = allergies
+        self.bed_num = bed_num
